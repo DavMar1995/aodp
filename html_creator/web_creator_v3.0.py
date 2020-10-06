d35338567d892f5ab1d0019f3322e03625348cd8
yj <yj.willy.lin@gmail.com>
2020-10-06 16:59:07 +0800

    add onsert

--- html_creator/web_creator_v3.0.py
+++ html_creator/web_creator_v3.0.py
@@ -93,7 +93,7 @@ def insert_data():
 
     while True:
         code_list.append(insert_code())
-        if is_continuted('Another tableau board in this page? (Y/N)', 2):
+        if is_continuted('Another tableau board in this page? (Y/N)', times=2):
             break
 
     file = {
@@ -115,13 +115,14 @@ def insert_code():
         'code': code}
 
 
-def is_continuted():
-    cont = input()
-    if cont == 'Y' or cont == 'y':
-        return True
-    elif cont != 'N' or cont == 'n':
-        return False
-    else:
+def is_continuted(text, times=3):
+    for i in range(times):
+        cont = input(text)
+        if cont == 'Y' or cont == 'y':
+            return True
+        elif cont != 'N' or cont == 'n':
+            return False
+    return False
 
 
 if __name__ == '__main__':
@@ -140,6 +141,9 @@ if __name__ == '__main__':
     ]
 
     insert_data()
+
+    'Another tableau board in this page? (Y/N)'
+    # < div class ='tableauPlaceholder' id='viz1601974634471' style='position: relative' > < noscript > < a href='https:&#47;&#47;aodp.herokuapp.com' > < img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Re&#47;RemoteVisitorsofDataportal_asia&#47;RemoteVisitorsofDataportal_asia&#47;1_rss.png' style='border: none' / > < / a > < / noscript > < object class ='tableauViz'  style='display:none;' > < param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' / > < param name='embed_code_version' value='3' / > < param name='site_root' value='' / > < param name='name' value='RemoteVisitorsofDataportal_asia&#47;RemoteVisitorsofDataportal_asia' / > < param name='tabs' value='yes' / > < param name='toolbar' value='yes' / > < param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Re&#47;RemoteVisitorsofDataportal_asia&#47;RemoteVisitorsofDataportal_asia&#47;1.png' / > < param name='animate_transition' value='yes' / > < param name='display_static_image' value='yes' / > < param name='display_spinner' value='yes' / > < param name='display_overlay' value='yes' / > < param name='display_count' value='yes' / > < param name='language' value='en' / > < / object > < / div > < script type='text/javascript' > var divElement = document.getElementById('viz1601974634471');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth * 0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement); < / script >
 
     for file in FILE_DICT:
         get_file(file)
